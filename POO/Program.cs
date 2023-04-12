d159faf362fa72b5b30a1f223f244500a4eb28f9
Abel Castro <abelcastrohoyos@gmail.com>
2023-04-11 21:51:45 -0500

    Finish date class

---
+++ POO/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POO;
+
+Console.WriteLine("Hola Programación orientada a objetos!");
+Console.WriteLine("======================================");
+
+
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(2023, 2, 2));
+	Console.WriteLine(new Date(2023, 11, 31));
+}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.Message);
+}
+
